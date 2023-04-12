26bb67295ce8a909951cb82cb331259f8a6ab152
benedictebucher <59736883+benedictebucher@users.noreply.github.com>
2023-04-12 14:41:51 +0200

    Update hal.js
    remplacé UNDEFINED par OTHER

--- js/hal.js
+++ js/hal.js
@@ -66,7 +66,7 @@ const publication_options = {
   pubACTN:"&fq=popularLevel_s:0&fq=docType_s:\"COMM\"&fq=invitedCommunication_s:0&fq=proceedings_s:1&fq=audience_s:(NOT 2)",
   pubOS:  "&fq=popularLevel_s:0&fq=docType_s:\"COUV\"",
   pubDO:  "&fq=popularLevel_s:0&fq=docType_s:\"DOUV\"",
-  pubAP:  "&fq=popularLevel_s:0&fq=docType_s:(\"REPORT\" OR \"UNDEFINED\")",
+  pubAP:  "&fq=popularLevel_s:0&fq=docType_s:(\"REPORT\" OR \"OTHER\")",
   pubTH:  "&fq=popularLevel_s:0&fq=docType_s:(\"THESE\" OR \"HDR\")",
   pubAFF: "&fq=popularLevel_s:0&fq=docType_s:\"POSTER\""
 }
